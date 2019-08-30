066ecb70ca4cd1a17d623316ca8ccae1b1865009
Owen Oertell <owen.oertell@gmail.com>
2019-08-30 17:05:26 -0400

    Basic Code

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -1,31 +1,28 @@
 #include "main.h"
 
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
+#define BACK_LEFT_WHEEL_PORT 1
+#define BACK_RIGHT_WHEEL_PORT 2
+#define FRONT_LEFT_WHEEL_PORT 3
+#define FRONT_RIGHT_WHEEL_PORT 4
+
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
-		int right = master.get_analog(ANALOG_RIGHT_Y);
+	pros::Motor backLeft(BACK_LEFT_WHEEL_PORT);
+	pros::Motor frontLeft(BACK_LEFT_WHEEL_PORT);
+	pros::Motor backRight(BACK_RIGHT_WHEEL_PORT, true);
+	pros::Motor frontRight(BACK_RIGHT_WHEEL_PORT, true);
+	
+	int left, right;
 
-		left_mtr = left;
-		right_mtr = right;
+	while (true) {
+		left = master.get_analog(ANALOG_LEFT_Y);
+		right = master.get_analog(ANALOG_RIGHT_Y);
+
+		backLeft.move(left);
+		backRight.move(right);
+		frontLeft.move(left);
+		frontRight.move(right);
+
 		pros::delay(20);
 	}
 }
