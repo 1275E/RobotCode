7a7e7ffbb9194f54de67f1f2e7b4cb2812a7e2f5
Owen Oertell <owen.oertell@gmail.com>
2019-08-31 17:20:05 -0400

    Oops

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -6,11 +6,11 @@
 #define FRONT_RIGHT_WHEEL_PORT 4
 
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
+	/*pros::Controller master(pros::E_CONTROLLER_MASTER);
 	pros::Motor backLeft(BACK_LEFT_WHEEL_PORT);
-	pros::Motor frontLeft(BACK_LEFT_WHEEL_PORT);
+	pros::Motor frontLeft(FRONT_LEFT_WHEEL_PORT);
 	pros::Motor backRight(BACK_RIGHT_WHEEL_PORT, true);
-	pros::Motor frontRight(BACK_RIGHT_WHEEL_PORT, true);
+	pros::Motor frontRight(FRONT_RIGHT_WHEEL_PORT, true);
 	
 	int left, right;
 
@@ -24,5 +24,5 @@ void opcontrol() {
 		frontRight.move(right);
 
 		pros::delay(20);
-	}
+	}*/
 }
